28bdda8463fffea4eae80cb73aec16a6829eacd1
Mohamed9820m <mohamedhabibmarouani8@gmail.com>
2023-11-18 00:50:00 +0100

    done2

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
-// next.config.js
+const webpack = require('webpack'); // Add this line at the beginning
+
 module.exports = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(mp4|webm)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/videos/[name].[hash].[ext]',
-          },
+  webpack: (config, { isServer }) => {
+    // Enable Webpack build worker for both client and server
+    if (!isServer) {
+      config.plugins.push(new webpack.LoaderOptionsPlugin({ options: {} }));
+    }
+
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/videos/[name].[hash].[ext]',
         },
-      });
-  
-      return config;
-    },
-  };
-  
+      },
+    });
+
+    return config;
+  },
+};
